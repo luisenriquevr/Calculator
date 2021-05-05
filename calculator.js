4f323fa78b4941647ed3a204ca21d1e24414e605
gallegoloco <74375023+gallegoloco@users.noreply.github.com>
2021-05-05 12:46:37 +0200

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,21 @@
+function init(){
+	alert("This is the firt time I program in HTML, CSS or JS.\nThank you for this opportunity.");
+}
+function setResult(value) {
+    document.getElementById('result').innerHTML = value;
+}
+function getResult() {
+    return(document.getElementById('result').innerHTML);
+}
+function add(key) { 
+    var result = getResult();
+    if (result!='0' || isNaN(key)) setResult(result + key);
+    else setResult(key);
+}
+function calc() {
+    var result = eval(getResult()); 
+    setResult(result);
+}
+function del() { 
+    setResult(0);
+}
